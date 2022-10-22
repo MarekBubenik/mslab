aea50ea134c1199082635d6d0fe0356b87407fe8
Marek Bubeník <b_mar@tuta.io>
2022-10-22 17:30:38 +0200

    Couple of touch-ups in oneclick script.

--- PowerShell/General/start_programs_oneclick.ps1
+++ PowerShell/General/start_programs_oneclick.ps1
@@ -1,13 +1,16 @@
 # Start all listed programs with one click
 #
-#
 
-$apps = @(
+$coreApps = @(
     'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'
-    "C:\Users\$env:username\Appdata\Local\Microsoft\Teams\Update.exe" -ArgumentList '--processStart "Teams.exe"'
-    'C:\Progra, Files (x86)\Microsoft Office\root\Office16\OUTLOOK.EXE'
+    'C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK.EXE'
     'C:\Program Files (x86)\Microsoft Office\root\Office16\WINWORD.EXE'
     #'C:\Program Files\Mozilla Firefox\firefox.exe'
 )
 
-$apps | ForEach-Object { Start-Process $_ -nowait }
+$coreApps | ForEach-Object { Start-Process $_ }
+
+# Start Teams
+# Separated it because Teams being a b*tch and was throwing an error in %
+Start-Process "C:\Users\$env:username\Appdata\Local\Microsoft\Teams\Update.exe" -ArgumentList '--processStart "Teams.exe"'
+
