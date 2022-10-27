22f84fbb5e204edc29790cba79a7174ccbc8383e
Marek Bubeník <b_mar@tuta.io>
2022-10-27 21:46:38 +0200

    yee

---
+++ PowerShell/ServerSetup/ServerSetup0.ps1
@@ -0,0 +1,41 @@
+﻿<# 
+    Windows Server setup
+    Part 0
+ 
+
+#>
+
+# Prepare the image for template - sysprep, generic image, shutdown, out of box experience fresh install
+# %WINDIR%\system32\sysprep\sysprep.exe /generalize /shutdown /oobe
+
+Set-ExecutionPolicy RemoteSigned
+
+#region - Network configuration
+New-NetIPAddress -InterfaceAlias "Ethernet0" -IPAddress "192.168.254.2" -PrefixLength 24 -DefaultGateway "192.168.254.1"
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet0" -ServerAddresses "192.168.254.2","192.168.254.1", "8.8.8.8"
+
+#endregion
+
+#region - Time zone configuration
+Set-TimeZone -Id "Central Europe Standard Time"
+
+#endregion
+
+#region - Firewall rules configuration
+
+#File and Printer Sharing configuration
+Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)" -Enabled True
+Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv6-In)" -Enabled True
+
+#Network Discovery configuration
+Get-NetFirewallRule -DisplayGroup 'Network Discovery' | Set-NetFirewallRule -Profile 'Domain, Private, Public' -Enabled True 
+
+#endregion
+
+#region - Rename computer and restart
+Rename-Computer -NewName "AD001" -Restart
+
+#endregion
+
+
+
---
+++ PowerShell/ServerSetup/ServerSetup1.ps1
@@ -0,0 +1,15 @@
+﻿<# 
+    Windows Server setup
+    Part 1
+ 
+
+#>
+
+#region - Install Active Directory Domain Services
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+
+# Install new forest
+Install-ADDSForest -DomainName mblabcloudtwo.onmicrosoft.com -InstallDns
+
+Import-Module ADDSDeployment
+#endregion
